168845c7c630751184b0af8eccd613bd45e43919
Kaio Cesar <tecnico.kaio@gmail.com>
2014-07-13 13:01:39 -0300

    Criação do modulo Rest e aplicação do front com Bootstrap

---
+++ backend/src/web/curso/rest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import json
+from google.appengine.ext import ndb
+
+class Curso(ndb.Model):
+    nome = ndb.StringProperty()
+    preco = ndb.FloatProperty()
+
+def salvar(nome, preco):
+    preco = float(preco)
+    curso = Curso(nome=nome, preco=preco)
+    curso.put()
+
+def to_dict(c):
+        dct = c.to_dict()
+        dct['id'] = str(c.key.id())
+        return dct
+
+def listar(_resp):
+    query = Curso.query().order(-Curso.preco)
+    cursos = [to_dict(c) for c in query.fetch()]
+    cursos = json.dumps(cursos)
+    _resp.write(cursos)
+
+def listar_html(_write_tmpl):
+    query = Curso.query().order(Curso.preco)
+    cursos = [to_dict(c) for c in query.fetch()]
+    _write_tmpl("templates/cursos/listagem.html", {'cursos':cursos})
